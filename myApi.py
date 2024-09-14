94a5281695f735d38b1317c6f2f2457dbe52ddfd
Serenablack <sunsetsin1.sy@gmail.com>
2024-09-13 23:56:31 -0400

    feat: changes with post api

--- myApi.py
+++ myApi.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID, uuid4
 
 app=FastAPI()
+
+class Task(BaseMOdel):
+id:Optional[UUID]=None
+title:str
+description:Optional[str]=None
+completed:bool=False
+
+tasks=[]
  
-@app.get("/")
+@app.get("/tasks", response_model=List[Task])
 def getApi():
-  return {"user":"hey there"}
+  return tasks
+
+  @app.post("/tasks", response_model=Task)
+
+  def create-task(task:Task):
+    task.id=uuid4()
+    tasks.append(task)
+    return task
+
+if name=="__main__":
+  import uvicorn
