b8cc55299d0a8a0cc1787032d3251fed2b2c6dab
Serenablack <sunsetsin1.sy@gmail.com>
2024-09-14 23:52:48 -0400

    feat: get one api

--- myApi.py
+++ myApi.py
@@ -5,11 +5,11 @@ from uuid import UUID, uuid4
 
 app=FastAPI()
 
-class Task(BaseMOdel):
-id:Optional[UUID]=None
-title:str
-description:Optional[str]=None
-completed:bool=False
+class Task(BaseModel):
+  id:Optional[UUID]=None
+  title:str
+  description:Optional[str]=None
+  completed:bool=False
 
 tasks=[]
  
@@ -17,12 +17,27 @@ tasks=[]
 def getApi():
   return tasks
 
-  @app.post("/tasks", response_model=Task)
+@app.post("/tasks", response_model=Task)
 
-  def create-task(task:Task):
+def create_task(task:Task):
     task.id=uuid4()
     tasks.append(task)
     return task
 
-if name=="__main__":
+
+@app.get("/tasks/{task_id}",response_model=Task)
+def getOneApi(task_id:UUID):
+fortask in tasks:
+  if task.id==task_id
+  return task
+@app.put("/tasks", response_model=Task)
+
+def update_task(task:Task)
+tasks.append(task)
+return task
+
+if __name__=="__main__":
   import uvicorn
+
+
+
