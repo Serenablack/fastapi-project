5a36e16cf4df64c3e2acee534403b7e925c56691
Serenablack <sunsetsin1.sy@gmail.com>
2024-10-06 23:50:08 -0400

    refactor

--- socialmediaapi/main.py
+++ socialmediaapi/main.py
@@ -1,21 +1,7 @@
 from fastapi import FastAPI
 
-from socialmediaapi.models.post import UserPost, UserPostIn
+from socialmediaapi.router.post import router
 
 app = FastAPI()
 
-post_table = {}
-
-
-@app.get("/", response_model=list[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
-
-
-@app.post("/", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.model_dump()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
+app.include_router(router)
