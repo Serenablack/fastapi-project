34e2a06152ebf2859bddfb8952918f21397784c7
Serenablack <sunsetsin1.sy@gmail.com>
2024-10-11 23:22:39 -0400

    feat: prod and test config class and getconfig function

--- socialmediaapi/config.py
+++ socialmediaapi/config.py
@@ -1,9 +1,12 @@
+from functools import lru_cache
 from typing import Optional
 
 from pydantic import BaseSettings
 
 
 class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+
     class Config:
         env_file: str = ".env"
 
@@ -15,4 +18,26 @@ class GlobalConfig(BaseConfig):
 
 class DevConfig(GlobalConfig):
     class Config:
-        env_prefix: str = "Dev"
+        env_prefix: str = "Dev_"
+
+
+class ProdConfig(GlobalConfig):
+    class Config:
+        env_prefix: str = "Prod_"
+
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL = "sqlite:///test.db"
+    DB_FORCE_ROLL_BACK = True
+
+    class Config:
+        env_prefix: str = "Test_"
+
+
+@lru_cache()
+def get_config(env_state: str):
+    configs = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
+    return configs[env_state]()
+
+
+config = get_config(BaseConfig().ENV_STATE)
